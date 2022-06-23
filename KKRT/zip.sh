0f90aae2425f8ad3eb02100e95b6c142013bd63a
neel0210 <neeljadhav0210@gmail.com>
2022-06-23 20:25:39 +0530

    KKRT: Misc fixes

--- KKRT/zip.sh
+++ KKRT/zip.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-zip -r9 "Kakarot-Kernel-Realme6pro-Neel0210-$(date +"%d-%m-%Y-%H-%M").zip" META-INF tools anykernel.sh zImage-dtb version
-rm zImage-dtb
+zip -r9 "Kakarot-Kernel-Realme6pro-Neel0210-$(date +"%d-%m-%Y-%H-%M").zip" META-INF tools anykernel.sh zImage-dtb version Image.gz-dtb
+rm Image.gz-dtb
