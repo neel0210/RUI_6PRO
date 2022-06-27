279318fa26edcdc05c20dd77eb8ef0f547466a32
neel0210 <neeljadhav0210@gmail.com>
2022-06-27 13:53:16 +0530

    Build: Seprate RUI and AOSP Building BUMP version 1.2

--- KKRT/zip.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-rm -rf *.zip
-zip -r9 "Kakarot-Kernel-Realme6pro-Neel0210-$(date +"%d-%m-%Y-%H-%M").zip" META-INF tools anykernel.sh zImage-dtb version Image.gz-dtb
-rm Image.gz-dtb
--- build.sh
+++ build.sh
@@ -24,8 +24,17 @@ END(){
 	echo "Done!"
 }
 
+CLEAN(){
+	make clean && make mrproper
+	rm -rf out/arch/arm64/boot/Image.gz-dtb
+	rm -rf KKRT/*.zip
+	rm -rf KKRT/Image.gz-dtb
+	rm -rf out
+}
+
 # Enforcing
-RUI="AOSP_atoll_defconfig"
+RUI="RUI_atoll_defconfig"
+AOSP="AOSP_atoll_defconfig"
 clear
 echo "---------------------------"
 echo checking if bulding offline
@@ -68,37 +77,124 @@ rm -rf CORE/*.zip
 # If other device make change here
 ############################################
 clear
-echo "==============="
-echo "Building Clean"
-echo "==============="
-echo Clean build leftovers
-START
-make clean && make mrproper
-rm -rf out/arch/arm64/boot/Image.gz-dtb
-rm -rf KKRT/*.zip
-rm -rf KKRT/Image.gz-dtb
-clear
-echo "==================="
-echo "Building Clean for "
-echo "==================="
-make $RUI O=out CC=clang
-make -j$(nproc --all) O=out CC=clang
-echo
-if [ -f "$Image" ]; then
-	echo "Image compiled; packing it"
-	cp -r ./out/arch/arm64/boot/Image.gz-dtb ./KKRT/Image.gz-dtb
+
+RUI(){
+	echo "==================="
+	echo "Building RUI Clean"
+	echo "==================="
+	echo Clean build leftovers
+	START
+	make clean && make mrproper
+	rm -rf out/arch/arm64/boot/Image.gz-dtb
 	rm -rf KKRT/*.zip
-	cd KKRT
-	. zip.sh
-	cd ..
-	changelog=`cat KKRT/changelog.txt`
-	for i in KKRT/*.zip
-	do
-	curl -F "document=@$i" --form-string "caption=$changelog" "https://api.telegram.org/bot${BOT_TOKEN}/sendDocument?chat_id=${CHAT_ID}&parse_mode=HTML"
-	done
-	echo ""
-	END
-else
-    echo "Kernel isnt compiled, letting Neel know"
-    curl -F text="Realme 6 pro: Kernel is not compiled, come and check @neel0210" "https://api.telegram.org/bot${BOT_TOKEN}/sendMessage?chat_id=${CHAT_ID}&parse_mode=Markdown"
+	rm -rf KKRT/Image.gz-dtb
+	clear
+	echo "==================="
+	echo "Building Clean for "
+	echo "==================="
+	make $RUI O=out CC=clang
+	make -j$(nproc --all) O=out CC=clang
+	echo
+	if [ -f "$Image" ]; then
+		echo "Image compiled; packing it"
+		cp -r ./out/arch/arm64/boot/Image.gz-dtb ./KKRT/Image.gz-dtb
+		rm -rf KKRT/*.zip
+		cd KKRT
+		. Rzip.sh
+		cd ..
+		changelog=`cat KKRT/changelog.txt`
+		for i in KKRT/*.zip
+		do
+		curl -F "document=@$i" --form-string "caption=$changelog" "https://api.telegram.org/bot${BOT_TOKEN}/sendDocument?chat_id=${CHAT_ID}&parse_mode=HTML"
+		done
+		echo ""
+		END
+		cd KKRT
+		mv version Aversion
+		mv changelog.txt Achangelog.txt
+		cd ..
+	else
+	    echo "Kernel isnt compiled, letting Neel know"
+	    curl -F text="Realme 6 pro: RUI Kernel is not compiled, come and check @neel0210" "https://api.telegram.org/bot${BOT_TOKEN}/sendMessage?chat_id=${CHAT_ID}&parse_mode=Markdown"
+	fi
+}
+
+AOSP(){
+	echo "==================="
+	echo "Building AOSP Clean"
+	echo "==================="
+	echo Clean build leftovers
+	START
+	make clean && make mrproper
+	rm -rf out/arch/arm64/boot/Image.gz-dtb
+	rm -rf KKRT/*.zip
+	rm -rf KKRT/Image.gz-dtb
+	clear
+	echo "==================="
+	echo "Building Clean for "
+	echo "==================="
+	make $AOSP O=out CC=clang
+	make -j$(nproc --all) O=out CC=clang
+	echo
+	if [ -f "$Image" ]; then
+		echo "Image compiled; packing it"
+		cp -r ./out/arch/arm64/boot/Image.gz-dtb ./KKRT/Image.gz-dtb
+		rm -rf KKRT/*.zip
+		cd KKRT
+		. Azip.sh
+		cd ..
+		changelog=`cat KKRT/changelog.txt`
+		for i in KKRT/*.zip
+		do
+		curl -F "document=@$i" --form-string "caption=$changelog" "https://api.telegram.org/bot${BOT_TOKEN}/sendDocument?chat_id=${CHAT_ID}&parse_mode=HTML"
+		done
+		echo ""
+		END
+		cd KKRT
+		mv version Aversion
+		mv changelog.txt Achangelog.txt
+		cd ..
+	else
+	    echo "Kernel isnt compiled, letting Neel know"
+	    curl -F text="Realme 6 pro: AOSP Kernel is not compiled, come and check @neel0210" "https://api.telegram.org/bot${BOT_TOKEN}/sendMessage?chat_id=${CHAT_ID}&parse_mode=Markdown"
+	fi
+}
+
+clear
+echo "                                                     "
+echo " _   __ _   ________ _____                 _       _     "
+echo "| | / /| | / /| ___ \_   _|  ___  ___ _ __(_)_ __ | |_ "
+echo "| |/ / | |/ / | |_/ / | |   / __|/ __| '__| | '_ \| __|"
+echo "|    \ |    \ |    /  | |   \__ \ (__| |  | | |_) | |_  "
+echo "| |\  \| |\  \| |\ \  | |   |___/\___|_|  |_| .__/ \__|"
+echo "\_| \_/\_| \_/\_| \_| \_/                   |_|         "
+echo "            coded by Neel0210  "
+echo "Select"
+echo "1 = Clean Build"
+echo "2 = Clean RUI"
+echo "3 = Clean AOSP"
+echo "4 = Update dependencies"
+echo "5 = exit"
+read n
+
+if [ $n -eq 1 ]; then
+	CLEAN
+fi
+#
+if [ $n -eq 2 ]; then
+	RUI
+fi
+#
+if [ $n -eq 3 ]; then
+	AOSP
+fi
+#
+if [ $n -eq 4 ]; then
+	sudo bash scripts/bin
+fi
+#
+if [ $n -eq 5 ]; then
+	echo "Quiting now"
+	sleep 2
+	exit
 fi
